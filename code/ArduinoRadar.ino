afd1c70ac1c7a9c0634cfaa1ebfd6d0c369acd34
anushreyaB29 <anushreyab29@gmail.com>
2026-05-31 12:56:00 +0530

    Upload ArduinoRadar.ino

---
+++ code/ArduinoRadar.ino
@@ -0,0 +1,89 @@
+#include <Servo.h>
+
+Servo myServo;
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzzerPin = 8;
+const int ledRed = 6;
+const int ledGreen = 5;
+
+long duration;
+int distance;
+
+void setup() {
+ Serial.begin(9600);
+
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+
+ pinMode(buzzerPin,OUTPUT);
+ pinMode(ledRed,OUTPUT);
+
+ myServo.attach(11);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int angle = 0; angle <= 140; ++angle){
+    myServo.write(angle);
+    
+    delay(30);
+
+    distance = calculateDistance();
+
+    if(distance > 0 && distance < 20){
+      digitalWrite(ledRed,HIGH);
+      digitalWrite(ledGreen,LOW);
+      digitalWrite(buzzerPin,HIGH);
+    }
+    else{
+      digitalWrite(buzzerPin,LOW);
+      digitalWrite(ledRed,LOW);
+      digitalWrite(ledGreen,HIGH);
+    }
+    
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+
+  for(int angle = 140; angle >= 0; --angle){
+    myServo.write(angle);
+    delay(30);
+
+    distance = calculateDistance();
+
+    if(distance > 0 && distance < 20){
+      digitalWrite(ledRed,HIGH);
+      digitalWrite(ledGreen,LOW);
+      digitalWrite(buzzerPin,HIGH);
+    }
+    else{
+      digitalWrite(buzzerPin,LOW);
+      digitalWrite(ledGreen,HIGH);
+      digitalWrite(ledRed,LOW);
+    }
+    
+
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+}
+
+int calculateDistance(){
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin,LOW);
+
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration * 0.034 / 2;
+  return distance;
+}
